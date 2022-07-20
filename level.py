5c2b5d8f6cdf630fd7207b22c8967e374c0c94ac
mbarker0907 <mbarker0907@gmail.com>
2022-07-21 01:01:13 +0200

    World Map 1

--- level.py
+++ level.py
@@ -1,5 +1,7 @@
 import pygame
 from settings import*
+from tile import Tile
+from player import Player
 
 class Level:
     def __init__(self):
@@ -12,9 +14,15 @@ class Level:
         self.obstacles_sprites = pygame.sprite.Group()
 
         #sprite setup
+        self.create_map()
+
     def create_map(self):
-        for row in WORLD_MAP:
+        for row_index, row in enumerate(WORLD_MAP):
+            print(row_index)
             print(row)
+            
+                
+
 
     def run(self):
         # update and draw the game
